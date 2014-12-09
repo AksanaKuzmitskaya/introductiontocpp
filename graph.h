78dcc1f7cc116d244fa487989acbbc59fa3af92a
Aksana Kuzmitskaya <aksana.kuzmitskaya@gmail.com>
2014-12-08 21:00:38 -0500

    Create graph.h

---
+++ graph.h
@@ -0,0 +1,23 @@
+/**
+ * graph.h
+ *
+ * Aksana Kuzmitskaya
+ * aksana.kuzmitskaya@gmail.com
+ *
+ * Graph class that uses STL containers (vectors) to represent a directed graph.
+ */
+ 
+#include <map>
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+class Graph {
+  map<int, vector<int> > list;
+  
+ public:
+  Graph(const vector<int> &starts, const vector<int> &ends);
+  int numOutgoing(const int nodeID) const;
+  const vector<int> &adjacent(const int nodeID) const;    
+};
